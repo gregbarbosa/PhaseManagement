a85a6f2c156605edaa3e35560eac90e0ba0f6551
gregbarbosa <gregbarbosa@me.com>
2014-11-17 02:01:22 -0500

    * Integrated Fabric into application * Lowered version number to match future dev plans

--- ArtistsView/AppDelegate.swift
+++ ArtistsView/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: true)
-        
+        Fabric.with([Crashlytics()])
         return true
     }
 
